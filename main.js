a0f6a8fec176313d21354839be9be80bebb1d093
freddyasp21 <freddyperozo21@gmail.com>
2025-07-04 14:42:09 -0400

    feat: Add dynamic calorie input fields and basic validation

--- main.js
+++ main.js
@@ -12,4 +12,47 @@ const output = document.getElementById("output");
 
 let isError = false;
 
-function cleanInputString(str) {}
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+  
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  
+  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
+  `;
+
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+addEntryButton.addEventListener("click", addEntry);
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+}
